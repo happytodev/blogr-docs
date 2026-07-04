1e1f21d7dad7fd278f7e561d8dc53b7f1067d289
Frédéric Blanc <frederic.blanc@itanea.fr>
2026-07-04 17:57:16 +0200

    fix: render save button in DocsSettings page

--- resources/views/docs-settings.blade.php
+++ resources/views/docs-settings.blade.php
@@ -1,15 +1,5 @@
-@php
-    use Filament\Support\Facades\FilamentView;
-@endphp
-
 <x-filament::page>
-    <x-filament::card>
-        {{ $this->form }}
-    </x-filament::card>
+    {{ $this->form }}
 
-    <x-filament::card>
-        <div class="flex justify-end">
-            {{ $this->getAction('save') }}
-        </div>
-    </x-filament::card>
+    <x-filament::footer.actions :actions="$this->getFormActions()" />
 </x-filament::page>
